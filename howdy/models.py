0ae08af76f97c4152aa29c0d0c8ff6d09ffa621d
Nicolas Gordillo <nicolas.gordillo@swisscom.com>
2019-03-29 15:51:00 +0100

    Without project model

--- howdy/models.py
+++ howdy/models.py
@@ -1,17 +1,5 @@
 from django.db import models
 
-class Project(models.Model):
-    project_name = models.CharField(max_length=255)
-    owner = models.CharField(max_length=255, default='')
-    git_url = models.CharField(max_length=255, unique=True)
-    project_dir = models.CharField(max_length=255)
-    master_branch = models.CharField(max_length=255)
-    branches = models.CharField(max_length=255, default='')
-
-    def __str__(self):
-        """Return a human readable representation of the model instance."""
-        return "{}".format(self.name)
-
 class Commit(models.Model):
     commitId = models.CharField(max_length=255, unique=True, primary_key=True)
     branchName = models.CharField(max_length=255, default='')
