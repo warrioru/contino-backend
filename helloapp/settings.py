947283d2006bd0b633925655d5bd794b4a000373
Nicolas Gordillo <nicolas.gordillo@swisscom.com>
2019-07-10 10:19:30 +0200

    remove conn max age

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -147,5 +147,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
+prod_db  =  dj_database_url.config()
 DATABASES['default'].update(prod_db)
